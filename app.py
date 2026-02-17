389eb27bc38e127d20d79d505fe338668ec0d6ab
Jai <jaiddeshpande@gmail.com>
2026-02-17 11:38:43 +0800

    Initial commit without large file

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ movies=pd.DataFrame(movies_dict)
 similarity=pickle.load(open('similarity.pkl','rb'))
 st.title("Movie Recommender System")
 
+
 selected_movie_name=st.selectbox(
     'Movies',
     movies['title'].values)
